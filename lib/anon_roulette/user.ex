2128a94fc5bda9a22b9034875161624c6d91420e
JD Mauthe <jd@jdmauthe.com>
2023-12-19 14:01:17 -0800

    Fixed user table associations

--- lib/anon_roulette/user.ex
+++ lib/anon_roulette/user.ex
@@ -16,22 +16,22 @@ defmodule AnonRoulette.User do
     belongs_to :ethnicity, AnonRoulette.Ethnicity, foreign_key: :ethnic_id, references: :ethnic_id
 
     # FK from profile_pictures table
-    belongs_to :profile_picture, AnonRoulette.ProfilePictures, foreign_key: :profile_picture_id
+    belongs_to :profile_picture, AnonRoulette.ProfilePicture, references: :profile_picture_id
 
     # FK for reports table
     has_many :sender_reports, AnonRoulette.Report, foreign_key: :sender_id
     has_many :offender_reports, AnonRoulette.Report, foreign_key: :offender_id
 
     # FK for private_messages table
-    has_many :messages_from, AnonRoulette.PrivateMessages, foreign_key: :from_user
-    has_many :messages_to, AnonRoulette.PrivateMessages, foreign_key: :to_user
+    has_many :messages_from, AnonRoulette.PrivateMessage, foreign_key: :from_user_id
+    has_many :messages_to, AnonRoulette.PrivateMessage, foreign_key: :to_user_id
 
     # FK for friend_requests table
     has_many :requests_from, AnonRoulette.FriendRequest, foreign_key: :requester_id
     has_many :requests_to, AnonRoulette.FriendRequest, foreign_key: :receiver_id
 
     # FK and PK for many-to-many users_roulette table
-    many_to_many :session, AnonRoulette.Session,
+    many_to_many :session, AnonRoulette.RouletteSession,
       join_through: "users_roulette",
       join_keys: [user_id: :user_id, roulette_session_id: :roulette_session_id]
 
