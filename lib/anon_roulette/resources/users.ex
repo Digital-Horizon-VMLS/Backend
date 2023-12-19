89ce8575e5975d6dac4df4c78bc8199a7a43c3d5
JD Mauthe <jd@jdmauthe.com>
2023-12-19 14:38:58 -0800

    Added documentation to user resource module

--- lib/anon_roulette/resources/users.ex
+++ lib/anon_roulette/resources/users.ex
@@ -1,7 +1,13 @@
 defmodule AnonRoulette.Resources.Users do
+  @moduledoc """
+  Provides methods to preform CRUD operations on the user resource.
+  """
   alias AnonRoulette.User
   alias AnonRoulette.Repo
 
+  @doc """
+  Get user using either id or email
+  """
   def get_user(id) when is_integer(id) do
     case Repo.get(User, id) do
       nil -> {:error, :not_found}
@@ -18,12 +24,34 @@ defmodule AnonRoulette.Resources.Users do
     end
   end
 
+  @doc """
+  Create a user using the map given as argument. The map can use
+  either strings or atoms as keys. Mixed type keys are not allowed.
+
+  Example attrs:
+    %{
+        username: "johndoe",
+        email: "johndoe@email.com"
+    }
+  """
   def create_user(attrs) do
     %User{}
     |> User.create_changeset(attrs)
     |> Repo.insert()
   end
 
+  @doc """
+  Updates a user with matching id using the map given as the 2nd argument.
+  The map can use either strings or atoms as keys. Mixed type keys
+  are not allowed.
+
+  Example attrs:
+    %{
+        first_name: "John",
+        last_name: "",
+        profile_description: "I have no last name"
+    }
+  """
   def update_user(id, attrs) when is_integer(id) do
     with {:ok, user} <- get_user(id) do
       user
@@ -32,6 +60,9 @@ defmodule AnonRoulette.Resources.Users do
     end
   end
 
+  @doc """
+  Deactivates the user with the matching id
+  """
   def deactivate_user(id) when is_integer(id) do
     with {:ok, user} <- get_user(id) do
       user
