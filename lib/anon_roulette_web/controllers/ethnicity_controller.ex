b988de197f72191e7de18753bab0e9fb00111411
aden73 <35171116+aden73@users.noreply.github.com>
2023-12-30 23:23:03 -0800

    Created resources module for ethnicities and allowed fallback controller to handle errors

--- lib/anon_roulette_web/controllers/ethnicity_controller.ex
+++ lib/anon_roulette_web/controllers/ethnicity_controller.ex
@@ -1,24 +1,23 @@
 defmodule AnonRouletteWeb.EthnicityController do
   use AnonRouletteWeb, :controller
-  alias AnonRoulette.{Repo, Ethnicity}
-  action_fallback AnonRouletteWeb.ErrorController
+  alias AnonRoulette.Resources.Ethnicities
 
   def index(conn, _params) do
-    ethnicities = Repo.all(Ethnicity)
+    ethnicities = Ethnicities.all_ethnicities()
     render(conn, :index, ethnicities: ethnicities)
   end
 
   def show(conn, %{"ethnicity_id" => id}) do
     case Integer.parse(id) do
-      {number, ""} when is_integer(number) ->
-        case Repo.get(Ethnicity, number) do
-          nil ->
+      {number, _} when is_integer(number) ->
+        case Ethnicities.get_ethnicity(number) do
+          {:ok, ethnicity} ->
+            render(conn, :show, ethnicity: ethnicity)
+          
+          {:error, :not_found} ->
             conn
             |> put_status(:not_found)
             |> render(:"404", message: "Ethnicity not found")
-
-          ethnicity ->
-            render(conn, :show, ethnicity: ethnicity)
         end
 
       _ ->
